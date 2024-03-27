ce78b2f20086709a392f5367b6ba89dcd507d92b
farah-emn <farahemad2001@gmail.com>
2024-03-27 21:15:30 +0300

    f1

--- lib/ui/shared/colors.dart
+++ lib/ui/shared/colors.dart
@@ -5,17 +5,13 @@ class AppColors {
   static const mainColorBlue = Color.fromRGBO(71, 148, 254, 1);
   static const Color blackColor = Color.fromRGBO(70, 77, 83, 1);
   static const Color pinkColor = Color.fromRGBO(239, 142, 230, 1);
-static const LightBlueColor = Color.fromARGB(255, 199, 223, 255);
-static const StatusBarColor = Color.fromARGB(255, 182, 214, 255);
-static const LightGrayColor = Color.fromARGB(255, 249, 249, 249);
-static const TextgrayColor = Color.fromARGB(255, 170, 170, 170);
-static  const TextBlackColor = Color.fromARGB(255, 30, 30, 30);
-static const backgroundgrayColor = Color.fromARGB(255, 249, 249, 249);
-static const TextFieldcolor = Color.fromARGB(255, 255, 255, 255);
-static const IconPurpleColor = Color.fromARGB(255, 175, 153, 255);
-static const IconBlueColor = Color.fromARGB(255, 162, 210, 255);
-
-
-
-
+  static const LightBlueColor = Color.fromARGB(255, 199, 223, 255);
+  static const StatusBarColor = Color.fromARGB(255, 182, 214, 255);
+  static const LightGrayColor = Color.fromARGB(255, 249, 249, 249);
+  static const TextgrayColor = Color.fromARGB(255, 170, 170, 170);
+  static const TextBlackColor = Color.fromARGB(255, 30, 30, 30);
+  static const backgroundgrayColor = Color.fromARGB(255, 249, 249, 249);
+  static const TextFieldcolor = Color.fromARGB(255, 255, 255, 255);
+  static const IconPurpleColor = Color.fromARGB(255, 175, 153, 255);
+  static const IconBlueColor = Color.fromARGB(255, 162, 210, 255);
 }
